c588c7aee6ac50394f95cf30ac09151e10ec3b25
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-11 14:13:37 -0400

    building formReducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,8 +1,9 @@
-const INITIAL_STATE = {
+// By default user is not authenticated and by default there is no error message
+
+const INITIAL_STATE = { // all caps because it is a truly constant variable
     authenticated: '',
     errorMessage: ''
 };
-
 
 export default function(state = INITIAL_STATE, action) {
     return state;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'; // renaming import statement with as helper
 import auth from './auth';
 
 export default combineReducers({
-    auth
+    auth,
+    form: formReducer
 });
