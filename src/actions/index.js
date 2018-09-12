e406e6394425270dbbefc648fea92f9a402f89db
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-12 18:12:57 -0400

    reordered imports

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,6 @@
-const API_SERVER = "http://localhost:8080";
-
-
 import axios from 'axios';
 import { AUTH_USER } from './types';
-
+const API_SERVER = "http://localhost:8080";
 // export const signup  = ({ email, password }) => { // creating an action creator called signup
 //     return { // inside action creator we always return an object that has a type property and a payload
 //         type: auth_user, // action creators usually return an action that is sent to redux thunk middleware, is then sent to the reducers, and reducers produce our news state that flows back into our components
