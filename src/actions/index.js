cd20c880e4f145eeb3dd0bfd5ebfcb8958cabb6b
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-18 05:52:40 -0400

    added fetch_student

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,20 @@
+// Write function to retrieve lots of json
+// Make an ajax request - somehow
+
 import axios from 'axios';
-import { AUTH_USER, AUTH_ERROR } from './types';
+import { AUTH_USER, AUTH_ERROR, FETCH_STUDENT } from './types';
 
 export const signup = (formProps, callback) => async dispatch => {
     try {
         const response = await axios.post('http://localhost:8080/signup', formProps);
-        dispatch({type: AUTH_USER, payload: response.data.token});
+        dispatch({ type: AUTH_USER, payload: response.data.token });
 
         // store JWT token
 
         localStorage.setItem('jwtToken', response.data.token);
         callback();
     } catch(e) {
-        dispatch({ type: AUTH_ERROR, payload: 'Email is in use'});
+        dispatch({ type: AUTH_ERROR, payload: 'This email is in use'});
     }
 };
 
@@ -25,6 +28,12 @@ export const signin = (formProps, callback) => async dispatch => {
     }
 };
 
+const fetchStudent = async () => {
+    const res = await axios.get('http://localhost:8080/students');
+    const json = await res.json();
+    return json;
+};
+
 export const signout = () => {
     localStorage.removeItem('jwtToken');
     return {
@@ -32,5 +41,7 @@ export const signout = () => {
         payload: '' // clear authenticated piece of state
     };
 };
+
+
 
 
