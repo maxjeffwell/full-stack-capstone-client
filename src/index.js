75bd3d9578cdaf6a9ec7ff5534261fdbd21a095e
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-18 05:55:59 -0400

    added more detailed comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// data layer control for redux (app root) - start up redux side of things in the app, render root component but limit react configuration here
+
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+// BrowserRouter tells react router what to do - looks at current url and changes components visible on screen
+// Route is a react component used to set a rule between a certain route in the application and a set of components that will be available on screen
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -12,11 +19,14 @@ import Register from './components/auth/Register';
 import Students from './components/auth/Students';
 import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
+import Header from "./components/Header";
 
 
 const store = createStore(
     reducers,
+
     // use starting state object to get initializing state inside redux store, pass to store the key of auth piece of state and then value to be initalized when redux store is created (authenticated) and assign to it whatever is returns from localStorage
+
     {
         auth: { authenticated: localStorage.getItem('jwtToken') }
     },
@@ -24,10 +34,18 @@ const store = createStore(
 );
 
 ReactDOM.render (
+
+    // ReactDOM - two arguments - root component and where we want to render that component inside of the DOM
+    // root component is the app component
+    // create redux store at top level of app and connect it to react by placing provider tag
+    // provider is a react component (provided by react-redux store) that can read changes from redux store
+    // anytime redux store state changes the provider component informs all of its children components
+
     <Provider store={store}>
     <BrowserRouter>
     <App>
-        <Route path='/' exact component={Landing} />
+        <Header />
+        <Route exact path='/' component={Landing} />
         <Route path='/signup' component={Register} />
         <Route path='/students' component={Students} />
         <Route path='/signin' component={Signin} />
@@ -35,6 +53,6 @@ ReactDOM.render (
     </App>
     </BrowserRouter>
     </Provider>,
-        document.querySelector('#root')
+        document.querySelector('#root') // have to pass in reference to root
 );
 
