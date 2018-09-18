145e149167391e91c69824e34990ec1a123fa24e
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-18 05:55:19 -0400

    edited comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
+// 2 separate reducers / 2 separate pieces of state
+// one reducer records whether the user is logged in
+// the other records the student data a user has fetched
+
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'; // renaming import statement with as helper
 import auth from './auth';
