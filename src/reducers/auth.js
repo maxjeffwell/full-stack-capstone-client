605e281434c25f1ff2d57d7c479df5656300ac6d
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-17 21:42:48 -0400

    attempting to resolve persisting error message

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -11,7 +11,7 @@ export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case AUTH_USER:
             return { ...state, authenticated: action.payload };
-        case AUTH_ERROR:
+            case AUTH_ERROR:
             return { ...state, errorMessage: action.payload };
             default:
             return state;
