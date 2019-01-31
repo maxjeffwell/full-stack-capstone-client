046cb1e8e7a2d7f01121d8855ce3a8f6f29e6bef
maxjeffwell <maxjeffwell@gmail.com>
2019-01-30 22:02:55 -0500

    work on signout component

--- src/components/auth/Signout.js
+++ src/components/auth/Signout.js
@@ -1,16 +1,17 @@
-// create class-based component in order to use lifecycle method
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
-class Signout extends Component {
+class Signout extends Component { // create class based component in order to use lifecycle method
     componentDidMount() {
-        this.props.signout(); // as soon as component is rendered call action creator and that signs user out of app by removing authenticated JWT out of reducer
+        this.props.signout();
+
+        // as soon as component is rendered call action creator and that signs user out of app by
+        // removing authenticated JWT from reducer
     }
     render() {
         return <div>You have been logged out</div>;
-    }
+    };
 }
 
 export default connect(null, actions)(Signout);
