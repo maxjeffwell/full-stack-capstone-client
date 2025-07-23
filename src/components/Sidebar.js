4b856c4811f7426be34d012110ec771348ca3934
Jeff M <maxjeffwell@gmail.com>
2025-07-23 19:27:39 -0400

    refactor: remove student links from sidebar
    - Remove 'Access Your Student List' link from sidebar
    - Remove 'Create A New Student' header from sidebar
    - Clean up unused imports and styled components
    - Simplify sidebar to empty state with minimal markup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
-
-const StyledLink = styled(Link)`
-  &&& a {
-    font-size: 1.2em;
-    font-family: 'Roboto', 'sans-serif';
-    font-weight: bold;
-    color: ${props => props.theme.blue};
-    text-align: center;
-    margin: auto;
-    padding-bottom: 50px;
-    white-space: nowrap;
-    justify-self: center;
-  }
-  & :hover,
-  :focus {
-    box-shadow:
-      0 12px 16px 0 rgba(0, 0, 0, 0.24),
-      0 17px 50px 0 rgba(0, 0, 0, 0.19);
-    text-decoration: underline;
-  }
-  & :visited,
-  :focus {
-    color: rebeccapurple;
-    text-decoration: none;
-  }
-`;
 
 const StyledGrid = styled(Grid)`
   &&& {
@@ -35,39 +8,11 @@ const StyledGrid = styled(Grid)`
   }
 `;
 
-const StyledHeader = styled(Header)`
-  &&& {
-    font-size: 1em;
-    font-family: 'Roboto', 'sans-serif';
-    font-weight: bold;
-    background-color: ${props => props.theme.blue};
-    border: 3px solid ${props => props.theme.orange};
-    color: ${props => props.theme.white};
-    border-radius: 5px;
-    padding: 0.25em;
-    text-align: center;
-    margin: auto;
-    width: fit-content;
-    white-space: nowrap;
-  }
-`;
-
 class SideBar extends Component {
   render() {
     return (
       <StyledGrid container columns={1} centered data-testid="sidebar">
-        <Grid.Row centered verticalAlign="middle">
-          <StyledLink as={Link} to="/students">
-            Access Your Student List
-          </StyledLink>
-        </Grid.Row>
-        <Grid.Row
-          centered
-          verticalAlign="middle"
-          style={{ paddingBottom: '0px' }}
-        >
-          <StyledHeader>Create A New Student</StyledHeader>
-        </Grid.Row>
+        {/* Sidebar content removed */}
       </StyledGrid>
     );
   }
